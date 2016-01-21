e585039d8eb30daca9ae73bc500662219073420f
llcoole93 <llcoole93@gmail.com>
2016-01-21 11:01:40 -0500

    Init Dev with new Account controllers

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return this.RedirectToAction("Login");
+        }
+
+        /// <summary>
+        /// To create account for my application
+        /// </summary>
+        /// <returns>ViewResult for the create</returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Logging users into the web site
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
